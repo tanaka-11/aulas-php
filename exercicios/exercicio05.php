feac0841538c0c9a601e7cee70e4786c4aef47e7
Marina Tanaka <huttenloucher@gmail.com>
2023-08-03 11:55:25 -0300

    codigo corrigido do exercicio 5

--- exercicios/exercicio05.php
+++ exercicios/exercicio05.php
@@ -4,9 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercicio 5 - Funções</title>
+
+<style>
+h1 {text-align: center;}
+
+</style>
+
 </head>
 <body>
-    <h1>Calculando Media de Notas</h1>
-    
+    <h1>Calculando Média de Notas</h1>
+    <?php
+        function soma($nota1,$nota2){
+            $media = ($nota1 + $nota2) / 2;
+            return $media;
+    }
+    ?>
+
+    <?php
+        $media = soma(10,5);  
+    ?>
+
+    <?php
+    function situacao($media) {
+        if ($media >= 7) {
+            return "Aprovado";
+        }
+            return "Reprovado";
+    }     
+    ?>
+
+    <p>Média: <?=$media?> </p> 
+    <p>Situação: <?=situacao($media)?></p>
+
+
 </body>
 </html>
