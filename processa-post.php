07feddeb99d33de644cf918334d18225366e9c27
Marina Tanaka <huttenloucher@gmail.com>
2023-08-08 10:36:21 -0300

    Mudança no formulario para o metodo post

---
+++ processa-post.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Processo _POST - PHP</title>
+</head>
+<body>
+    <h1>Exemplo utilizando o processamento _POST</h1>
+    <hr>
+    <p>Receber e processar dados via <b>_POST</b></p>
+    
+    <!-- Capturando dados transmitidos -->
+    
+
+    <h2>Dados:</h2>
+    <ul>
+        <li>Nome: </li>
+        <li>E-mail: </li>
+        <li>Mensagem: </li>
+    </ul>
+</body>
+</html>
