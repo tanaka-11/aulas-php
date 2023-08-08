47e08f697aa21ab99dc041bedc3faa126d0c4b94
Marina Tanaka <huttenloucher@gmail.com>
2023-08-08 08:33:28 -0300

    adicionado a data e hora atual no rodapé

--- 09-site/includes/rodape.php
+++ 09-site/includes/rodape.php
@@ -1,7 +1,12 @@
 </main>
-    
+    <?php
+        $data = date ("d/m/Y");
+        date_default_timezone_set("America/Sao_Paulo");
+        $hora = date("H:i:s");
+    ?>
+
     <footer>
-        <p>Site criado para o curso <b>Téc. Informática para Internet</b></p>
+        <p>Site criado para o curso <b>Téc. Informática para Internet</b> <b><?=$data?></b> <b><?=$hora?></b> </p>
     </footer>
 </body>
 </html>
