488874a3fd801bc1f01193663dfe67089303d9f8
Marina Tanaka <huttenloucher@gmail.com>
2023-08-08 10:31:43 -0300

    Adicionado exemplo de receber dados com _GET
    usado extract($_GET);

--- processa-get.php
+++ processa-get.php
@@ -1,3 +1,28 @@
-<?php
-var_dump($_GET)
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Processo _GET - PHP</title>
+</head>
+<body>
+    <h1>Exemplo utilizando o processamento _GET</h1>
+    <hr>
+    <p>Receber e processar dados via <b>_GET</b></p>
+    
+    <!-- Capturando dados transmitidos -->
+    <?php
+    // $nome = $_GET["nome"];
+    // $email = $_GET["email"];
+    // $mensagem = $_GET["mensagem"];
+    extract($_GET); 
+    ?>
+
+    <h2>Dados:</h2>
+    <ul>
+        <li>Nome: <?=$nome?></li>
+        <li>E-mail: <?=$email?></li>
+        <li>Mensagem: <?=$mensagem?></li>
+    </ul>
+</body>
+</html>
