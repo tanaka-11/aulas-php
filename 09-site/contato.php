ffb959d68b1a2be0fe4c02c28425dc8368297f5d
Marina Tanaka <huttenloucher@gmail.com>
2023-08-08 08:28:13 -0300

    criado a pagina de contato

---
+++ 09-site/contato.php
@@ -0,0 +1,3 @@
+<?php require "includes/cabecalho.php" ?>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, tempore beatae. Eligendi recusandae suscipit nulla quaerat laboriosam! Fuga nostrum sequi aliquid sit culpa, minus hic, autem eaque cumque, veniam ad.</p>
+<?php require "includes/rodape.php" ?>    
