a1ae19f1f9ea1cbb9cfea05f5025726952f6eecf
Marina Tanaka <huttenloucher@gmail.com>
2023-08-08 10:48:40 -0300

    adicionado o required nos campos do formulario

--- processa-post.php
+++ processa-post.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>Processo _POST - PHP</title>
+    
 </head>
 <body>
     <h1>Exemplo utilizando o processamento _POST</h1>
